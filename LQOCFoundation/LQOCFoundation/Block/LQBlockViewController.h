e9e053744f0b2411d323f05cd8511911e1ce2b5b
liang <957977361@qq.com>
2019-05-01 22:32:07 +0800

    add block demo test

---
+++ LQOCFoundation/LQOCFoundation/Block/LQBlockViewController.h
@@ -0,0 +1,17 @@
+//
+//  LQBlockViewController.h
+//  LQOCFoundation
+//
+//  Created by liang on 2019/5/1.
+//  Copyright © 2019 LQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LQBlockViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LQOCFoundation/LQOCFoundation/Block/LQBlockViewController.m
@@ -0,0 +1,69 @@
+//
+//  LQBlockViewController.m
+//  LQOCFoundation
+//
+//  Created by liang on 2019/5/1.
+//  Copyright © 2019 LQ. All rights reserved.
+//
+
+#import "LQBlockViewController.h"
+
+@interface LQBlockViewController ()
+
+@end
+
+@implementation LQBlockViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+//    [self testGlobalBlock];
+//    [self testMallocBlock];
+//    [self testStackBlock];
+    
+}
+
+- (void)testGlobalBlock {
+    
+    static int static_var = 3;
+    void (^globalBlock)(void) = ^{
+        static_var++;
+        NSLog(@"Block var:%d", static_var);
+    };
+    
+    //__NSGlobalBlock__
+    NSLog(@"%@", globalBlock);
+}
+
+- (void)testMallocBlock {
+    NSObject *obj = [NSObject new];
+    void (^mallocBlock)(void) = ^{
+        NSLog(@"Block obj:%@", obj);
+    };
+    
+    //__NSMallocBlock__
+    NSLog(@"%@", mallocBlock);
+}
+
+
+- (void)testStackBlock {
+    
+    int var = 3;
+    void (^mallocBlock)(void) = ^{
+        NSLog(@"Block var:%d", var);
+    };
+    //__NSMallocBlock__
+    NSLog(@"%@", mallocBlock);
+    
+    
+    __block int blockVar = 10;
+    //__NSStackBlock__
+    NSLog(@"%@", ^{NSLog(@"var: %d %p", blockVar++, &blockVar);});
+}
+
+
+@end
